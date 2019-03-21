ebaf74d4cce1e07647328c9e91d101a305072742
Florian Eickhorst <eickhorst@fleimedia.de>
2019-03-21 10:07:17 +0100

    sortable columns

--- acf_admin_columns.php
+++ acf_admin_columns.php
@@ -16,9 +16,9 @@ class FleiACFAdminColumns
     private static $instance;
 
     private $exclude_field_types = array(
-        'tab'   => 'tab',
-        'clone' => 'clone',
-        'repeater' => 'repeater'
+        'tab'      => 'tab',
+        'clone'    => 'clone',
+        'repeater' => 'repeater',
     );
 
     private $admin_columns = array();
@@ -34,8 +34,9 @@ class FleiACFAdminColumns
 
     private function __construct()
     {
-        add_action('acf/init', array($this, 'add_actions')); // add ACF fields
-        add_action('wp', array($this, 'prepare_columns'));
+        add_action('acf/init', array($this, 'add_acf_actions')); // add ACF fields
+        add_action('parse_request', array($this, 'prepare_columns'), 10);
+        add_action('pre_get_posts', array($this, 'action_prepare_query_sort'));
     }
 
     /**
@@ -43,7 +44,7 @@ class FleiACFAdminColumns
      */
     public function prepare_columns()
     {
-        if (!function_exists('acf_get_field_groups') || !function_exists('acf_get_fields')) {
+        if (!$this->is_acf_active()) {
             return;
         }
 
@@ -71,15 +72,42 @@ class FleiACFAdminColumns
             $this->admin_columns = apply_filters('acf/admin_columns/admin_columns', $this->admin_columns);
 
             if (!empty($this->admin_columns)) {
-                add_filter('manage_' . $ptype . '_posts_columns', array($this, 'filter_manage_posts_columns'));
-                add_action('manage_' . $ptype . '_posts_custom_column', array($this, 'action_manage_posts_custom_column'), 10, 2);
+                add_filter('manage_' . $ptype . '_posts_columns', array($this, 'filter_manage_posts_columns')); // creates the columns
+                add_filter('manage_' . $screen->id . '_sortable_columns', array($this, 'filter_manage_sortable_columns')); // make columns sortable
+                add_action('manage_' . $ptype . '_posts_custom_column', array($this, 'action_manage_posts_custom_column'), 10, 2); // outputs the columns values for each post
             }
         }
     }
 
+    /**
+     * prepares WPs query object when ordering by ACF field
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function action_prepare_query_sort($query)
+    {
+
+        $orderby = $query->query_vars['orderby'];
+
+        if (array_key_exists($orderby, $this->admin_columns)) {
+
+            // this makes sure we sort also when the custom field has never been set on some posts before
+            $meta_query = array(
+                'relation' => 'OR',
+                array('key' => $orderby, 'compare' => 'NOT EXISTS'), // 'NOT EXISTS' needs to go first for proper sorting
+                array('key' => $orderby, 'compare' => 'EXISTS'),
+            );
+
+            $query->set('meta_query', $meta_query);
+            $query->set('orderby', 'meta_value');
+        }
+
+        return $query;
+    }
 
     /**
-     * Adds the designated columns to Wordpress admin post list table
+     * Adds the designated columns to Wordpress admin post list table.
      *
      * @param $columns array passed by Wordpress
      * @return array
@@ -89,6 +117,24 @@ class FleiACFAdminColumns
         if (!empty($this->admin_columns)) {
             $columns = array_merge($columns, $this->admin_columns);
         }
+
+        return $columns;
+    }
+
+    /**
+     * Makes our columns rendered as sortable.
+     *
+     * @param $columns
+     * @return mixed
+     */
+    public function filter_manage_sortable_columns($columns)
+    {
+
+        foreach ($this->admin_columns as $idx => $acol) {
+            $columns[($idx)] = $idx;
+        }
+
+        $columns = apply_filters('acf/admin_columns/sortable_columns', $columns);
 
         return $columns;
     }
@@ -110,16 +156,15 @@ class FleiACFAdminColumns
         }
     }
 
-
     /**
-     * Add ACF hooks
+     * Add ACF hooks and handle versions
      */
-    public function add_actions()
+    public function add_acf_actions()
     {
-        $exclude = apply_filters('acf/user_role_setting/exclude_field_types', $this->exclude_field_types);
-        if (!function_exists('acf_get_setting')) {
+        if (!$this->is_acf_active()) {
             return;
         }
+        $exclude = apply_filters('acf/admin_columns/exclude_field_types', $this->exclude_field_types);
         $acf_version = acf_get_setting('version');
         $sections = acf_get_field_types();
         if ((version_compare($acf_version, '5.5.0', '<') || version_compare($acf_version, '5.6.0', '>=')) && version_compare($acf_version, '5.7.0', '<')) {
@@ -150,6 +195,11 @@ class FleiACFAdminColumns
             'instructions' => 'Show this field as a column in the admin post list.',
         );
         acf_render_field_setting($field, $args, false);
+    }
+
+    private function is_acf_active()
+    {
+        return (function_exists('acf_get_field_groups') && function_exists('acf_get_fields'));
     }
 
 }
