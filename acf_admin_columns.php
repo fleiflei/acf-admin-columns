127885413ddaca16b0c5f91b9dc63bcdda3247a1
Florian Eickhorst <eickhorst@fleimedia.de>
2019-03-27 16:36:36 +0100

    rendering of column fields

--- acf_admin_columns.php
+++ acf_admin_columns.php
@@ -16,9 +16,14 @@ class FleiACFAdminColumns
     private static $instance;
 
     private $exclude_field_types = array(
-        'tab'      => 'tab',
-        'clone'    => 'clone',
-        'repeater' => 'repeater',
+        'accordion',
+        'clone',
+        'flexible_content',
+        'google_map',
+        'group',
+        'message',
+        'repeater',
+        'tab',
     );
 
     private $admin_columns = array();
@@ -88,19 +93,21 @@ class FleiACFAdminColumns
     public function action_prepare_query_sort($query)
     {
 
-        $orderby = $query->query_vars['orderby'];
+        if ($query->query_vars && isset($query->query_vars['orderby'])) {
+            $orderby = $query->query_vars['orderby'];
 
-        if (array_key_exists($orderby, $this->admin_columns)) {
+            if (array_key_exists($orderby, $this->admin_columns)) {
 
-            // this makes sure we sort also when the custom field has never been set on some posts before
-            $meta_query = array(
-                'relation' => 'OR',
-                array('key' => $orderby, 'compare' => 'NOT EXISTS'), // 'NOT EXISTS' needs to go first for proper sorting
-                array('key' => $orderby, 'compare' => 'EXISTS'),
-            );
+                // this makes sure we sort also when the custom field has never been set on some posts before
+                $meta_query = array(
+                    'relation' => 'OR',
+                    array('key' => $orderby, 'compare' => 'NOT EXISTS'), // 'NOT EXISTS' needs to go first for proper sorting
+                    array('key' => $orderby, 'compare' => 'EXISTS'),
+                );
 
-            $query->set('meta_query', $meta_query);
-            $query->set('orderby', 'meta_value');
+                $query->set('meta_query', $meta_query);
+                $query->set('orderby', 'meta_value');
+            }
         }
 
         return $query;
@@ -148,11 +155,122 @@ class FleiACFAdminColumns
     public function action_manage_posts_custom_column($column, $post_id)
     {
         if (array_key_exists($column, $this->admin_columns)) {
-            $field_value = get_field($column, $post_id);
+//            $field_value = $this->render_column_field($column, $post_id);
 
+            $field_value = acf_format_value(get_field($column), $post_id, true);
             $field_value = apply_filters('acf/admin_columns/column/' . $column, $field_value);
 
             echo $field_value;
+        }
+    }
+
+    public function render_column_field($column, $post_id)
+    {
+
+        $field_value = get_field($column, $post_id);
+        if ($field_value) {
+            $render_output = '';
+
+            $field_properties = acf_get_field($column, $post_id);
+            $field_images = $field_value;
+            $items_more = 0;
+
+            switch ($field_properties['type']) {
+                case 'color_picker':
+                    $render_output .= '<div style="height:20px;width:100%;display:inline-block;background-color:' . $field_value . '">' . $field_value . '</div><br>';
+                    break;
+                case 'taxonomy': //@todo
+                    $render_output = $field_value;
+                    break;
+                case 'file':
+                    $render_output = $field_value['filename'] ?: ''; // @todo multiple values
+                    break;
+                case 'wysiwyg':
+                    $render_output = wp_trim_excerpt(strip_tags($field_value));
+                    break;
+                case 'link':
+                    if (is_array($field_value) && isset($field_value['url'])) {
+                        $render_output = $field_value['url'];
+                    }
+                    break;
+                case 'post_object':
+                case 'relationship':
+                    $p = $field_value;
+                    if (is_array($field_value) && !empty($field_value)) {
+                        $p = $field_value[0];
+                        $items_more = count($field_value) - 1;
+                    }
+                    $render_output = '<a href="' . get_edit_post_link($p, false) . '">' . $p->post_title . '</a>';
+
+                    break;
+                case 'user':
+                    $u = $field_value;
+                    if (is_array($field_value) && !empty($field_value)) {
+                        $u = $field_value[0];
+                        $items_more = count($field_value) - 1;
+                    }
+                    $render_output = '<a href="' . get_edit_user_link($u) . '">' . $u['display_name'] . '</a>';
+                    break;
+                case 'image':
+                    $field_images = array($field_value);
+                case 'gallery':
+
+                    if (!empty($field_images)) {
+                        $preview_image = $field_images[0]; // use first image as preview
+                        $preview_image_id = 0;
+                        $preview_image_url = '';
+
+                        if (is_array($field_value) && isset($preview_image['ID'])) {
+                            $preview_image_id = $preview_image['ID'];
+                        } else if (intval($preview_image) > 0) {
+                            $preview_image_id = intval($preview_image);
+                        }
+
+                        if (filter_var($preview_image, FILTER_VALIDATE_URL)) {
+                            $preview_image_url = $preview_image;
+                        } else if ($preview_image_id > 0) {
+                            $img = wp_get_attachment_image_src($preview_image_id, 'thumbnail');
+                            if (is_array($img) && isset($img[0])) {
+                                $preview_image_url = $img[0];
+                            }
+                        }
+
+                        if ($preview_image_url) {
+                            $render_output = "<img style='width:100%;height:auto;' src='$preview_image_url'>";
+                        }
+
+                        $items_more = count($field_value) - 1;
+                    }
+                    break;
+
+                case 'text':
+                case 'textarea':
+                case 'number':
+                case 'range':
+                case 'email':
+                case 'url':
+                case 'password':
+                case 'select':
+                case 'checkbox':
+                case 'radio':
+                case 'button_group':
+                case 'true_false':
+                case 'page_link':
+                case 'date_picker':
+                case 'time_picker':
+                default:
+                    $render_output = $field_value;
+            }
+
+            if (is_array($render_output)) {
+                $render_output = implode(', ', $render_output);
+            }
+
+            if ($items_more) {
+                $render_output .= "<br>and $items_more more";
+            }
+
+            return $render_output;
         }
     }
 
@@ -170,7 +288,7 @@ class FleiACFAdminColumns
         if ((version_compare($acf_version, '5.5.0', '<') || version_compare($acf_version, '5.6.0', '>=')) && version_compare($acf_version, '5.7.0', '<')) {
             foreach ($sections as $section) {
                 foreach ($section as $type => $label) {
-                    if (!isset($exclude[$type])) {
+                    if (!in_array($type, $exclude)) {
                         add_action('acf/render_field_settings/type=' . $type, array($this, 'render_field_settings'), 1);
                     }
                 }
@@ -178,7 +296,7 @@ class FleiACFAdminColumns
         } else {
             // >= 5.5.0 || < 5.6.0
             foreach ($sections as $type => $settings) {
-                if (!isset($exclude[$type])) {
+                if (!in_array($type, $exclude)) {
                     add_action('acf/render_field_settings/type=' . $type, array($this, 'render_field_settings'), 1);
                 }
             }
